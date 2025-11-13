efc86639babc0195169fc1b33a218ed1e1ed4039
Andrew Brown <ab36245@gmail.com>
2025-11-13 19:20:59 +1100

    Don't generate error for unimplemented param parser

--- params.go
+++ params.go
@@ -1,7 +1,5 @@
 package cli
 
-import "fmt"
-
 type Params []*Param
 
 func (p *Params) Init() {
@@ -11,12 +9,12 @@ func (p *Params) Init() {
 }
 
 func (p *Params) Parse(args *[]string) error {
+	// TODO
 	// for _, param := range *p {
 	// 	err := param.Binding.Consume(args, param.Min, param.Max)
 	// 	if err != nil {
 	// 		return err
 	// 	}
 	// }
-	// return nil
-	return fmt.Errorf("unimplemented")
+	return nil
 }
